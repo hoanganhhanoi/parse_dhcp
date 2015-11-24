0bfc037c6d040ece919a24b255fa053bdbe62a0e
Nguyen hoang anh <anhnh@vccloud.vn>
2015-11-24 13:50:49 +0700

    write file

--- lib/parse_dhcp/host.rb
+++ lib/parse_dhcp/host.rb
@@ -2,9 +2,10 @@
 class Host
 
   attr_reader :host, :hardware_ethernet, :fix_address
-  def initialize
-    @host = ""
-    @hardware_ethernet = ""
-    @fix_address = ""
+  
+  def initialize(host, hardware_ethernet, fix_address)
+    @host = host
+    @hardware_ethernet = hardware_ethernet
+    @fix_address = fix_address
   end
 end
--- lib/parse_dhcp/net.rb
+++ lib/parse_dhcp/net.rb
@@ -1,7 +1,10 @@
+require_relative "writeconf.rb"
+require_relative "host.rb"
 
 class Net
 
-  attr_reader :subnet, :netmask, :option, :differ, :pool
+  attr_accessor :subnet, :netmask, :option, :differ, :pool
+
   def initialize
     @subnet = ""
     @netmask = ""
@@ -9,7 +12,46 @@ class Net
     @differ = {}
     @pool = { "range" => "",
               "allow" => "", 
-              "hosts" => {}
+              "hosts" => []
             }
   end
+
+  def test
+    net1 = Net.new
+    net2 = Net.new
+    # Set subnet
+    net1.subnet = "192.168.1.0"
+    net1.netmask = "255.255.255.0"
+    # Set option
+    net1.option["routers"] = "192.168.1.1"
+    net1.option["subnet-mask"] = "255.255.255.0"
+    net1.option["broadcast-address"] = "192.168.1.255"
+    net1.option["domain-name-servers"] = "194.168.4.100"
+    net1.differ["authoritative"] = "true"
+    net1.differ["default-lease-time"] = "2"
+    net1.differ['max-lease-tim'] = "86400"
+    # Set pool
+    net1.pool["range"] = { "min" => "192.168.25.20", "max" => "192.168.25.200" }
+    net1.pool["allow"] = "unknown-clients"
+    net1.pool["hosts"] << Host.new("bla1", "DD:GH:DF:E5:F7:D7", "192.168.1.2")
+    net1.pool["hosts"] << Host.new("bla2", "00:JJ:YU:38:AC:45", "192.168.1.20")
+
+    # Set subnet
+    net2.subnet = "192.168.1.0"
+    net2.netmask = "255.255.255.0"
+    # Set option
+    net2.option["routers"] = "192.168.1.1"
+    net2.option["subnet-mask"] = "255.255.255.0"
+    net2.option["broadcast-address"] = "192.168.1.255"
+    net2.option["domain-name-servers"] = "194.168.4.100"
+    net2.differ["default-lease-time"] = "2"
+    net2.differ['max-lease-tim'] = "86400"
+    # Set pool
+    net2.pool["range"] = { "min" => "192.168.25.20", "max" => "192.168.25.200" }
+    net2.pool["allow"] = "unknown-clients"
+    net2.pool["hosts"] << Host.new("bla1", "DD:GH:DF:E5:F7:D7", "192.168.1.2")
+    net2.pool["hosts"] << Host.new("bla2", "00:JJ:YU:38:AC:45", "192.168.1.20")
+    return [net1,net2]
+  end
+
 end
